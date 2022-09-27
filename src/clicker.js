d6db862b3f2f78bec4118d7c74b27ee80d4f1332
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-09-27 19:54:14 +0200

    first version

---
+++ src/clicker.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Clicker extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Number is </h1>
+                <button>Random number</button>                
+            </div>
+        )
+    }
+}
+
+export default Clicker;
